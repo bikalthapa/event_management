3a479fc1fcb72e7ac47562b2c3460ce93567ffb5
bikalthapa <bikalt40@gmail.com>
2025-04-06 22:39:05 +0545

    Design was revised

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,93 +1,66 @@
-(function($, ElementQueries) {
-    document.addEventListener("arlojscontrolsloaded", function() {
-        var platformID = "websitetestdata.arlo.co"; // Change platformID to point at your own account
-
-        var filter = {
-            moduleType: "Filters",
-            targetElement: "#filters",
-            template: "#filter-template",
-            filterControlId: 1
-        };
-
-        var eventList = {
-            moduleType: "UpcomingEvents",
-            targetElement: "#upcoming-events",
-            template: "#upcoming-events-template",
-            maxCount: 12,
-            filterControlId: 1,
-            includeLoadMoreButton: true,
-            loadMoreButtonText: "Show More",
-            includeArloLink: false,
-            smartDateFormats: {
-                startDay: "DD"
-            },
-            customUrls: {
-                eventtemplate: "/workshop/",
-                venue: "/venue/",
-                presenter: "/presenter/"
-            },
-            callbacks: {
-                onShow: eventListOnShowCallback
-            }
-        };
-
-        var app = new window.ArloWebControls();
-
-        app.start({
-            platformID: platformID,
-            showDevErrors: false,
-            modules: [eventList, filter]
-        });
-
-        /* ----- Callback function ----- */
-
-        // "OnShow" callback
-        function eventListOnShowCallback(getEventListItemsFunction) {
-            var listItems = getEventListItemsFunction();
-
-            var cardSummary = $('.arlo-summary');
-            var strMaxLength = 350;
-            
-            $.each(cardSummary, function (index, ele) {
-                var str = $(ele).text()
-                $(ele).text(strChopper(str));
-            });
-
-            function strChopper(str) {
-                if (str.length > strMaxLength) {
-                    str = str.substring(0, strMaxLength) + '...';
-                }
-                return str;
-            }
-            
-            ElementQueries.init();
-
-            // Hide timezone selector if there are no online events
-            if ($(".arlo-online").length < 1) {
-                $(".arlo-timezone-select").hide();
-            } else {
-                $(".arlo-timezone-select").show();
-                $(".arlo-timezone-select").parent().css("float", "right");
-            }
-
-            // Set up tooltips
-            $.each(listItems, function(index, listItem) {
-                var tooltipElement = $(listItem).find('[data-toggle="tooltip"]')[0];
-                if (tooltipElement) {
-                    var toolTipContent = $(listItem).find(".tooltipcontent")[0];
-                    if (toolTipContent) {
-                        $(tooltipElement).attr(
-                            "data-original-title",
-                            $(toolTipContent).html()
-                        );
-                        $(tooltipElement).tooltip();
-                    }
-                }
-            });
+let modal_body = document.getElementById("modal_body");
+let portfolioData;
+fetch('assets/data/portfolio.json') // Correct relative path from the HTML file
+    // ${item.description.map(desc => `<b>${desc}</b><br><br>`).join('')}
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        portfolioData = data.portfolio; // Access the "portfolio" array in the JSON
+        if (!Array.isArray(portfolioData)) {
+            throw new Error('Invalid data format: Expected an array');
         }
 
-        $("#arlo-filter-toggle").click(function() {
-            $(this).parent().toggleClass("arlo-show-filter");
+        const container = document.querySelector('.portfolio_section'); // Ensure this element exists in your HTML
+        if (!container) {
+            throw new Error('Portfolio section element not found in the DOM');
+        }
+
+        portfolioData.forEach((item, indx) => {
+            // Use template literals to create the card HTML
+            const cardHTML = `
+                <div class="col">
+                `+card_design(item,indx, true)+`
+                </div>
+            `;
+
+
+            // Append the card to the container
+            container.innerHTML += cardHTML;
         });
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
     });
-})(jQuery, window.ElementQueries);
+
+function card_design(item, indx, btnStatus) {
+    return `
+    <div class="card h-100">
+        <img src="${item.image}" class="card-img-top" alt="${item.title}">
+        <div class="card-body">
+            <h3 class="card-title portfolio_title">${item.title}</h3>
+            <p class="portfolio_date">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-event" viewBox="0 0 16 16">
+                    <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
+                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+                </svg>&nbsp;&nbsp; ${item.date}
+            </p>
+            <p class="portfolio_location">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                </svg>&nbsp;&nbsp; ${item.location}
+            </p>
+            ${btnStatus === false ? `<p class="card-text">${item.description.map(desc => `<b>${desc}</b><br><br>`).join('')}</p`:``}
+            ${btnStatus === true ? `<button onclick="read_portfolio(${indx})" class="CTA_solid w-100 mt-3" data-bs-toggle="modal" data-bs-target="#portfolioModal">Read More</button>` : ''}
+        </div>
+    </div>
+`;
+
+}
+function read_portfolio(indx) {
+    modal_body.innerHTML = card_design(portfolioData[indx],indx,false);
+}
